084dca9d6a1c295ccc12705389ff8a08b4d73891
subzeroice <139818196@qq.com>
2016-11-01 22:01:52 +0800

    创建java文件之HelloWorld
    第一次使用github来创建java文件

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+  System.out.print("HelloWorld");
+		System.out.println("欢迎大家来关注");
+		System.out.println("今天天气虽然太冷，但心情还是不错的");
+	}
+	
+}
